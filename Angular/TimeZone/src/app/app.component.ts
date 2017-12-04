34d6e39104aeb41d8c457727a47dfb73b3641edb
ninap41 <ninapalumbo@gmail.com>
2017-12-03 19:59:40 -0600

    Added Time Display

--- Angular/TimeZone/src/app/app.component.ts
+++ Angular/TimeZone/src/app/app.component.ts
@@ -6,14 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TimeZone!';
+  title = 'US TimeZones!';
   today = new Date();
-  timeZone = null;
+  timeZone = ''; // empty variable to plug timezone into
+  Region =""
+  color : String = 'blue';
 
-  onButtonClick(Zone) { 
+  FindZone(Zone) { 
     this.today = new Date();
-    if timezone ==='MST'
+
+    if (Zone ==='MST'){
+      // this.today  = Date.now();
+      this.today.setHours(this.today.getHours() -1);
+      this.Region="Mountain Standard";
+      this.color = 'blue';
+    }
+    else if(Zone ==='PST'){
+      this.today.setHours(this.today.getHours() -2);
+      this.Region="Pacific Standard";
+      this.color = 'green';
+        
+    }
+    else if (Zone ==='EST'){
+      this.today.setHours(this.today.getHours() +2);
+      this.Region="Eastern Standard";
+      this.color = 'red';
+      }
+      else if (Zone ==='CST'){
+        this.today.setHours(this.today.getHours() +0);
+        this.Region="Central Standard"; 
+        this.color = 'orange';
+        
+        }
+       
+     else{
+      this.Region ="";
+      this.timeZone  = Zone;
+      this.color = 'black';
+        }
+
+      
+       
+        
+        
+    }
+ 
+
   }
-}
 
 
